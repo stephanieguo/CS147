b08812bf4e13b7ff8628fbf231cf7c366ec0be42
stephanieguo <stguo@stanford.edu>
2012-10-17 14:19:19 -0700

    lab4

--- week3/index.php
+++ week3/index.php
@@ -17,7 +17,18 @@
 		
 		<table>
 		<!-- This is where we'll put all our content -->
-		
+<?php
+    include("config.php");
+    $query = "";
+    
+    // Don’t be intimidated by the following lines. You can pretty much
+    // always copy and paste these because they’re sort of like
+    // functional bits that never change.
+    $result = mysql_query($query);
+    while ($row = mysql_fetch_assoc($result)) {
+        
+    }
+    ?>
 		</table>
 		
 		<script type="text/javascript">
